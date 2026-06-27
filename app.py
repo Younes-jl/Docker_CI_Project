da85287dc3f462d61675adfa10936b15723c8cf0
Younes-jl <younesachlouj@gmail.com>
2026-06-27 15:45:19 +0100

    Add GitHub Actions workflow modification fix errors

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bienvenue sur mon premier projet CI/CD avec Docker et GitHub Actions devops !"
+    return "Bienvenue sur mon premier projet CI/CD avec Docker et GitHub Actions devops1 !"
 
 
 @app.route("/About")
